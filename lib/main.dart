fea9a2096c657cd7d2d4517891374c43fc282414
Udit Bhardwaj <114299379+TheUditBhardwaj@users.noreply.github.com>
2024-02-05 00:50:19 +0530

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:start_nivesh/Splash_Screen.dart';
+import 'package:start_nivesh/pages/On_Boarding_Screen.dart';
+
 
 import 'HomeScreen.dart';
 
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
+      home: OnBoarding(),
       debugShowCheckedModeBanner: false,
     );
   }
