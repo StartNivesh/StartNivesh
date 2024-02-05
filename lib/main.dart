273fb9050a8744462e2350df60fca432df5f40aa
Udit Bhardwaj <114299379+TheUditBhardwaj@users.noreply.github.com>
2024-02-05 23:47:42 +0530

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: OnBoarding(),
       debugShowCheckedModeBanner: false,
     );
   }
