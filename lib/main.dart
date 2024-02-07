56d86c336dc37f98f873eed54d16be5bf7c55f0c
StartNivesh <158811886+StartNivesh@users.noreply.github.com>
2024-02-08 01:56:56 +0530

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: StartupScreen(),
+      home: HomeScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
