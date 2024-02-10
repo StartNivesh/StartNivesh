434c9dd15657c0f6acc2774d2c9ffb7a19b3f260
Udit Bhardwaj <114299379+TheUditBhardwaj@users.noreply.github.com>
2024-02-10 14:26:07 +0530

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 import 'package:start_nivesh/ProfileScreen.dart';
@@ -11,10 +13,21 @@ import 'package:start_nivesh/pages/SignupScreen.dart';
 import 'package:start_nivesh/pages/StartupScreen.dart';
 
 
+
 import 'HomeScreen.dart';
 
-void main() {
-
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid?await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBhcOup5ijlTUFg2bCYd19aOdvqmpbtUp0",
+      appId: "1:794401066985:android:122e3727d8b6d24ad2c578",
+      messagingSenderId: "794401066985",
+      projectId: "flutter-firebase-demo-ebefa",
+    ),
+  )
+      :
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -22,13 +35,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: SignupScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 
-//None hatana h SignupScreen se
+//Firebase hogya
+
 
 
