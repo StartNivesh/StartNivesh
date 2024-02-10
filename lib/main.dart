cbfd62b2c7bbe15be870fcd283de5c3152373edc
StartNivesh <158811886+StartNivesh@users.noreply.github.com>
2024-02-10 14:21:30 +0530

    Test1

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginScreen(),
+      home: SignupScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/SignupScreen.dart
+++ lib/pages/SignupScreen.dart
@@ -1,3 +1,121 @@
+// import 'package:flutter/material.dart';
+//
+// class SignupScreen extends StatelessWidget {
+//   const SignupScreen({Key? key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: SingleChildScrollView(
+//         child: Center(
+//           child: Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Hero(
+//                   tag: 'logo',
+//                   child: Image.asset(
+//                     'assets/START.png',
+//                     height: 220,
+//                     width: 180,
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 Text(
+//                   'Sign Up',
+//                   style: TextStyle(
+//                     fontSize: 24,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Name',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(15.0),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 Row(
+//                   children: [
+//                     Expanded(
+//                       child: TextField(
+//                         decoration: InputDecoration(
+//                           labelText: 'Age',
+//                           border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(15.0),
+//                           ),
+//                         ),
+//                         keyboardType: TextInputType.number,
+//                       ),
+//                     ),
+//                     SizedBox(width: 20),
+//                     Expanded(
+//                       child: DropdownButtonFormField<String>(
+//                         decoration: InputDecoration(
+//                           labelText: 'Gender',
+//                           border: OutlineInputBorder(
+//                             borderRadius: BorderRadius.circular(15.0),
+//                           ),
+//                         ),
+//                         value: 'None', // Set default value if needed
+//                         items: <String>['None','Male', 'Female', 'Other']
+//                             .map((String value) {
+//                           return DropdownMenuItem<String>(
+//                             value: value,
+//                             child: Text(value),
+//                           );
+//                         }).toList(),
+//                         onChanged: (newValue) {
+//                           // Handle dropdown value change
+//                         },
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextField(
+//                   decoration: InputDecoration(
+//                     labelText: 'Email ID',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(15.0),
+//                     ),
+//                   ),
+//                   keyboardType: TextInputType.emailAddress,
+//                 ),
+//                 SizedBox(height: 20),
+//                 TextField(
+//                   obscureText: true,
+//                   decoration: InputDecoration(
+//                     labelText: 'Password',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(15.0),
+//                     ),
+//                   ),
+//                 ),
+//                 SizedBox(height: 20),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     // Perform sign-up logic here
+//                     // For simplicity, let's just print a message for now
+//                     print('Sign Up button pressed');
+//                   },
+//                   child: Text('Sign Up'),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
 class SignupScreen extends StatelessWidget {
@@ -6,6 +124,7 @@ class SignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Center(
           child: Padding(
@@ -14,28 +133,28 @@ class SignupScreen extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Hero(
-                  tag: 'logo',
-                  child: Image.asset(
-                    'assets/START.png',
-                    height: 220,
-                    width: 180,
-                  ),
+                SizedBox(height: 60),
+                Image.asset(
+                  'assets/START.png',
+                  height: 120,
+                  width: 120,
                 ),
                 SizedBox(height: 20),
                 Text(
                   'Sign Up',
                   style: TextStyle(
-                    fontSize: 24,
+                    fontSize: 28,
                     fontWeight: FontWeight.bold,
+                    color: Colors.blue,
                   ),
                 ),
                 SizedBox(height: 20),
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Name',
+                    suffixIcon: Icon(Icons.person,size: 20),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
                 ),
@@ -46,8 +165,9 @@ class SignupScreen extends StatelessWidget {
                       child: TextField(
                         decoration: InputDecoration(
                           labelText: 'Age',
+                          prefixIcon: Icon(Icons.calendar_today,size: 20),
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
                         ),
                         keyboardType: TextInputType.number,
@@ -58,12 +178,13 @@ class SignupScreen extends StatelessWidget {
                       child: DropdownButtonFormField<String>(
                         decoration: InputDecoration(
                           labelText: 'Gender',
+                          prefixIcon: Icon(Icons.person_outline),
                           border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                            borderRadius: BorderRadius.circular(20.0),
                           ),
                         ),
                         value: 'None', // Set default value if needed
-                        items: <String>['None','Male', 'Female', 'Other']
+                        items: <String>['None', 'Male', 'Female', 'Other']
                             .map((String value) {
                           return DropdownMenuItem<String>(
                             value: value,
@@ -81,8 +202,9 @@ class SignupScreen extends StatelessWidget {
                 TextField(
                   decoration: InputDecoration(
                     labelText: 'Email ID',
+                    suffixIcon: Icon(Icons.email,size: 20),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
                   keyboardType: TextInputType.emailAddress,
@@ -92,8 +214,9 @@ class SignupScreen extends StatelessWidget {
                   obscureText: true,
                   decoration: InputDecoration(
                     labelText: 'Password',
+                    suffixIcon: Icon(Icons.lock,size: 20,),
                     border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
+                      borderRadius: BorderRadius.circular(20.0),
                     ),
                   ),
                 ),
@@ -104,7 +227,20 @@ class SignupScreen extends StatelessWidget {
                     // For simplicity, let's just print a message for now
                     print('Sign Up button pressed');
                   },
-                  child: Text('Sign Up'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 16), backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  child: Container(
+                    width: double.infinity,
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ),
                 ),
               ],
             ),
