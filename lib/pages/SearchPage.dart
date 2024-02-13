4e1ad19e08ea9a3773c4a0da577eacc53b8ef9e3
Priyanshu Kumar Singh <85292194+redhawk1303d@users.noreply.github.com>
2024-02-13 17:42:17 +0530

    Test1

--- lib/pages/SearchPage.dart
+++ lib/pages/SearchPage.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:start_nivesh/HomeScreen.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final List<Map<String, String>> _searchResults = [
+    {'name': 'Innovize Solutions', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'GrowthGenius Ventures', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'LaunchPad Innovations', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Prodigy Enterprises', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Nexus Innovations Co.', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Apex Ventures Group', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Catalyst Creations', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Visionary Ventures Inc.', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Momentum Labs', 'imageUrl': 'assets/profile_images/cat.png'},
+    {'name': 'Pinnacle Ventures Ltd.', 'imageUrl': 'assets/profile_images/cat.png'},
+  ];
+
+  List<Map<String, String>> _filteredResults = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _filteredResults = _searchResults;
+  }
+
+  void _filterSearchResults(String query) {
+    setState(() {
+      _filteredResults = _searchResults
+          .where((result) => result['name']!.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,55 +45,45 @@ class SearchPage extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.grey), // Add back arrow icon
+          icon: Icon(Icons.arrow_back, color: Colors.grey),
           onPressed: () {
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>BottomNavigationBarExample())); // Navigate back when the arrow is pressed
+            Navigator.pushReplacement(context,MaterialPageRoute(builder: (context)=>BottomNavigationBarExample()));
           },
         ),
         title: TextField(
+          onChanged: _filterSearchResults,
           decoration: InputDecoration(
             hintText: 'Search',
             hintStyle: TextStyle(color: Colors.grey),
             prefixIcon: Icon(Icons.search, color: Colors.grey),
             border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20), // Adjust border radius here
+              borderRadius: BorderRadius.circular(20),
               borderSide: BorderSide.none,
             ),
-            contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16), // Adjust padding as needed
+            contentPadding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
             fillColor: Colors.grey.shade200,
             filled: true,
           ),
-          style: TextStyle(color: Colors.black), // Change text color here
+          style: TextStyle(color: Colors.black),
         ),
       ),
-      body: ListView(
+      body: ListView.builder(
         padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        children: [
-          _buildSearchResultItem('Innovize Solutions'),
-          _buildSearchResultItem('GrowthGenius Ventures'),
-          _buildSearchResultItem('LaunchPad Innovations'),
-          _buildSearchResultItem('Prodigy Enterprises'),
-          _buildSearchResultItem('Nexus Innovations Co.'),
-          _buildSearchResultItem('Apex Ventures Group'),
-          _buildSearchResultItem('Catalyst Creations'),
-          _buildSearchResultItem('Visionary Ventures Inc.'),
-          _buildSearchResultItem('Momentum Labs'),
-          _buildSearchResultItem('Pinnacle Ventures Ltd.'),
-        ],
+        itemCount: _filteredResults.length,
+        itemBuilder: (context, index) {
+          final username = _filteredResults[index]['name'];
+          final imageUrl = _filteredResults[index]['imageUrl'];
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: AssetImage(imageUrl!),
+            ),
+            title: Text(username!),
+            onTap: () {
+              // Handle tapping on a search result
+            },
+          );
+        },
       ),
-    );
-  }
-
-  Widget _buildSearchResultItem(String username) {
-    return ListTile(
-      leading: CircleAvatar(
-        backgroundColor: Colors.grey.shade200,
-        child: Icon(Icons.person, color: Colors.grey),
-      ),
-      title: Text(username),
-      onTap: () {
-        // Handle tapping on a search result
-      },
     );
   }
 }
